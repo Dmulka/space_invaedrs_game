923edc62b041ff17043f37e36ca24295d4934def
Dan Mulka <danielmulka92@gmail.com>
2023-05-23 23:26:03 -0500

    shooter + movement

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
+// get gameBoard variable 
+const grid = document.querySelector('.board')
 
-const grid = document.querySelector('.grid')
+let currentShooterIndex = 292
 
-for (let i = 0; i < 225; i++) {
+let width = 15
+ 
+const alienInvaders = [
+    0,1,2,3,4,5,6,7,8,9,10,11,12,12,13,14,
+    15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,
+    30,31,32,33,34,35,36,37,38,39,40,41,42,43,44
+  ]
+
+
+for (let i = 0; i < 300; i++) {
     const square = document.createElement('div')
     grid.appendChild(square)
   }
   
-  const squares = Array.from(document.querySelectorAll('.grid div'))
+  const squares = Array.from(document.querySelectorAll('.board div'))
   
-  const alienInvaders = [
-    0,1,2,3,4,5,6,7,8,9,
-    15,16,17,18,19,20,21,22,23,24,
-    30,31,32,33,34,35,36,37,38,39
-  ]
   
   function draw() {
     for (let i = 0; i < alienInvaders.length; i++) {
@@ -27,40 +33,27 @@ for (let i = 0; i < 225; i++) {
 
 
 
-// // get gameBoard variable 
-// const gameBoard = document.querySelector('.gameBoard')
 
-// // width of board 
-// let width = 15
 
-// // create variable for all the individual square - and make an array?
+// width of board 
 
-// const squares = Array.from(gameBoard.querySelectorAll('div'));
 
-// // creates the divs for the 15 * 20 board 
-//  for (let i=0; i < 300; i++){
-// const square = document.createElement('div')
-// gameBoard.appendChild(square)
-// }
+// create variable for all the individual square - and make an array?
 
-// //    ***invaders***
+//    ***invaders***
 
-// // 1. arrrya the index positions the invaders will stat off iin
-// const invadeArray = [0,1,2,3,4,5,6,7,8,9]
+// 1. arrrya the index positions the invaders will stat off iin
 
-// // 2.  function that iterates over the invader index and passsed the value 
-// // into the squares array.  Creates new class (invader)
-// function makeInvader() {
-//     for (let i = 0; i < invadeArray.length; i++) {
-//       const squareIndex = invadeArray[i];
-//       squares[squareIndex].classList.add('invader');
-//     }
-//   }
+
+// 2.  function that iterates over the invader index and passsed the value 
+// into the squares array.  Creates new class (invader)
+
+
+
 // invader movement logic   
 
 // 1. define left edge (invaders sould start lef) in the invader array where is the first invader rekative to the left edge?
 
-//const leftEdge = 
 
 //2, define right. invader lenght -1 due to index nnumber.  last invades sould = lenth -1 
 
@@ -72,11 +65,40 @@ for (let i = 0; i < 225; i++) {
 
 ///   ***shoote***
 
-// 1. create shoote.  Hhoose a grid square at the bottim. 
 
-// 2. deviine left 
 
-//3.  define right 
+// 1. create shooter  Hhoose a grid square at the bottim.
+squares[currentShooterIndex].classList.add('shooter')
+
+//const shooterArray = [285, 286, 287, 288, 289, 290, 291, 292, 293, 294, 295, 296, 297, 298, 299]
+
+
+ function moveShooterLeft() {
+ if (currentShooterIndex % width !== 0){
+    squares[currentShooterIndex].classList.remove('shooter')
+    currentShooterIndex --;
+    squares[currentShooterIndex].classList.add('shooter')
+    }
+ }
+function moveShooterRight() {
+if ((currentShooterIndex + 1) % width !==0){
+    squares[currentShooterIndex].classList.remove('shooter')
+    currentShooterIndex ++
+    squares[currentShooterIndex].classList.add('shooter')
+    }
+}
+    document.addEventListener('keydown', (event) => {
+        if (event.key === 'ArrowLeft') {
+          moveShooterLeft();
+        } else if (event.key === 'ArrowRight') {
+          moveShooterRight();
+        }
+      });
+
+// projectile 
+function blaster {
+
+}
 
 
 
