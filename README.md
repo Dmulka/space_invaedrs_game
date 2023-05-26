6b5bcc8a88e21ae57dba00fce36f2a2679eafe52
Dan Mulka <danielmulka92@gmail.com>
2023-05-26 07:33:56 -0500

    win pop up

--- README.md
+++ README.md
@@ -6,8 +6,9 @@ For my first project I will be attempting to make my own version of the classic 
 * The invaders are represented by several rows of objects that move side to side and drop down.
 * The player acts as the “shooter” at the bottom of the game space. The shooter can only shoot up and is fixd to the bottom but like the invaders can also move side to side the width of the screen.
 
-## Where to begin
+## What went well 
 
-To get started. The game space must be created.  Similar to connect four it will be composed of a grid space where our player and invader element can move.
+
+
 
 ## Credits 
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // get gameBoard variable 
 const grid = document.querySelector('.board')
 
-const winDisplay = document.getElementById('winDisplay')
+const popText = document.getElementById('popText')
 
 let currentShooterIndex = 292
 
@@ -11,11 +11,11 @@ let removedInvaders = []
  
 const upperRowIndex = [0,1,2,3,4,5,6,7,8,9]
 
-const alienInvaders = [
-    0,1,2,3,4,5,6,7,8,9,10,11,12,12,13,14,
-    15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,
-    30,31,32,33,34,35,36,37,38,39,40,41,42,43,44
-  ]
+const alienInvaders = [0]
+  //   0,1,2,3,4,5,6,7,8,9,10,11,12,12,13,14,
+  //   15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,
+  //   30,31,32,33,34,35,36,37,38,39,40,41,42,43,44
+  // ]
 
 
 for (let i = 0; i < 300; i++) {
@@ -51,28 +51,12 @@ for (let i = 0; i < 300; i++) {
 // into the squares array.  Creates new class (invader)
 
 
-
-// invader movement logic   
-
-// 1. define left edge (invaders sould start lef) in the invader array where is the first invader rekative to the left edge?
-
-
-//2, define right. invader lenght -1 due to index nnumber.  last invades sould = lenth -1 
-
-//3.  define movement downward
-
-//4.  End game when in coontact with shoote.  If alienarray === shooter positioon end make?
-
-//5. remove alien when hit 
-
 ///   ***shoote***
 
 
 
 // 1. create shooter  Hhoose a grid square at the bottim.
 squares[currentShooterIndex].classList.add('shooter')
-
-//const shooterArray = [285, 286, 287, 288, 289, 290, 291, 292, 293, 294, 295, 296, 297, 298, 299]
 
 
  function moveShooterLeft() {
@@ -126,8 +110,11 @@ function moveBlast(e) {
                 
            } 
            if (removedInvaders.length === alienInvaders.length){
-            winDisplay.innerHTML = 'You Win'
+            popText.classList.toggle('show')
             clearInterval(projectileID)
+
+            
+
 
         } 
     }
